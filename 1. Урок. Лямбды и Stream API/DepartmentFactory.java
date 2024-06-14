a5fc2950f55ede491bdbf9ccabb1fde1d42fccce
Pakhomov Nikolay <pakhomovnikolay@gmail.com>
2024-06-14 08:35:20 +0300

    Изменено название класса генерации персонала: DepartmentFabric => DepartmentFactory

--- 1. Урок. Лямбды и Stream API/DepartmentFactory.java
+++ 1. Урок. Лямбды и Stream API/DepartmentFactory.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class DepartmentFabric {
+public class DepartmentFactory {
 
     private static Random random = new Random();
 
